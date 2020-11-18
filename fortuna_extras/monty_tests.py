eacd6859577684642fea28d2923061313641533e
BrokenShell <webmaster@sharpdesigndigital.com>
2020-11-17 16:14:29 -0800

    Test updates

--- fortuna_extras/monty_tests.py
+++ fortuna_extras/monty_tests.py
@@ -32,21 +32,25 @@ def lambda_monty_tests():
         lambda x: f"C: {d(x)}",
         lambda x: f"D: {d(x)}",
         lambda x: f"E: {d(x)}",
+        lambda x: f"F: {d(x)}",
+        lambda x: f"G: {d(x)}",
+        lambda x: f"H: {d(x)}",
     ))
-    distribution_timer(monty.flat_uniform, x=4)
-    distribution_timer(monty.front_linear, x=4)
-    distribution_timer(monty.middle_linear, x=4)
-    distribution_timer(monty.back_linear, x=4)
-    distribution_timer(monty.quantum_linear, x=4)
-    distribution_timer(monty.front_gauss, x=4)
-    distribution_timer(monty.middle_gauss, x=4)
-    distribution_timer(monty.back_gauss, x=4)
-    distribution_timer(monty.quantum_gauss, x=4)
-    distribution_timer(monty.front_poisson, x=4)
-    distribution_timer(monty.middle_poisson, x=4)
-    distribution_timer(monty.back_poisson, x=4)
-    distribution_timer(monty.quantum_poisson, x=4)
-    distribution_timer(monty.quantum_monty, x=4)
+    d_size = 2
+    distribution_timer(monty.flat_uniform, x=d_size)
+    distribution_timer(monty.front_linear, x=d_size)
+    distribution_timer(monty.middle_linear, x=d_size)
+    distribution_timer(monty.back_linear, x=d_size)
+    distribution_timer(monty.quantum_linear, x=d_size)
+    distribution_timer(monty.front_gauss, x=d_size)
+    distribution_timer(monty.middle_gauss, x=d_size)
+    distribution_timer(monty.back_gauss, x=d_size)
+    distribution_timer(monty.quantum_gauss, x=d_size)
+    distribution_timer(monty.front_poisson, x=d_size)
+    distribution_timer(monty.middle_poisson, x=d_size)
+    distribution_timer(monty.back_poisson, x=d_size)
+    distribution_timer(monty.quantum_poisson, x=d_size)
+    distribution_timer(monty.quantum_monty, x=d_size)
 
 
 if __name__ == "__main__":
--- fortuna_extras/multi_threading.py
+++ fortuna_extras/multi_threading.py
@@ -4,26 +4,21 @@ produces the same `random` number for all threads. Conversely, Fortuna is thread
 compatible and will correctly produce random results for all threads. """
 from Fortuna import random_range
 from random import randrange
-import concurrent.futures
-import time
+from concurrent.futures import ProcessPoolExecutor
 
 
 if __name__ == '__main__':
     rand_limit = 100  # Distribution range 0-99
     n_jobs = 8  # Total number of threads
 
+    print(f"\nMulti-threaded randrange/random_range test")
+
     print("\nRandom.randrange:")
-    start = time.perf_counter()
-    with concurrent.futures.ProcessPoolExecutor() as executor:
+    with ProcessPoolExecutor() as executor:
         results = executor.map(randrange, [rand_limit] * n_jobs)
     print(list(results))
-    finish = time.perf_counter()
-    print(f'Time: {round(finish-start, 2)} second(s)')
 
     print("\nFortuna.random_range:")
-    start = time.perf_counter()
-    with concurrent.futures.ProcessPoolExecutor() as executor:
+    with ProcessPoolExecutor() as executor:
         results = executor.map(random_range, [rand_limit] * n_jobs)
     print(list(results))
-    finish = time.perf_counter()
-    print(f'Time: {round(finish-start, 2)} second(s)')
